155ee86d88117299d9837ec2800c5c31ebbaf9c0
wugdance <sintes4ek@gmail.com>
2025-01-19 16:49:50 +0300

    Update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,14 @@
 return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'AlexvZyl/nordic.nvim',
+  --'AlexvZyl/nordic.nvim',
+  'slugbyte/lackluster.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
+
   init = function()
     -- Load the colorscheme here.
     vim.o.background = 'dark'
-    vim.cmd.colorscheme 'nordic'
+    -- vim.cmd.colorscheme 'nordic'
+    vim.cmd.colorscheme 'lackluster-hack'
 
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
