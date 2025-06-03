fc907b4f2169c69d0ddcdbe45764865a03296b3a
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-06-03 14:52:55 +0300

    Delete unused plugin.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,9 +4,6 @@ return { -- Collection of various small independent plugins/modules
     -- Better Around/Inside textobjects
     require('mini.ai').setup { n_lines = 500 }
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    require('mini.surround').setup()
-
     local statusline = require 'mini.statusline'
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
