8cdbc88737f8ddef97bd5b59e3bd8ad230867328
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-07-12 14:44:10 +0300

    disable some diagnostic for pyright

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -152,8 +152,9 @@ return {
           settings = {
             python = {
               analysis = {
+                autoSearchPaths = true,
                 diagnosticSeverityOverrides = {
-                  -- Doesn't work properly
+                  reportOptionalMemberAccess = 'none',
                   reportCallIssue = 'none',
                 },
               },
@@ -193,6 +194,7 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'pyright',
         'stylua', -- Used to format Lua code
         'isort', -- python import sorting
         'black', -- python code formatter
