fd3f3aa2cb4ba4dd2042859ec863f4a7f036e5a0
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-07-12 14:45:27 +0300

    change telescope strategy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,7 +43,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
       defaults = {
         sorting_strategy = 'ascending',
         path_display = { truncate = 3 },
-        layout_strategy = 'vertical',
+        layout_strategy = 'flex',
       },
       -- pickers = {}
       extensions = {
