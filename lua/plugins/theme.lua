571e0c7f14a14cdb24f0d29f0bfb0cece50b997c
wugdance <sintes4ek@gmail.com>
2025-07-27 21:18:33 +0300

    a little bit improve theme config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,15 @@
 return {
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  -- 'AlexvZyl/nordic.nvim',
-  -- 'slugbyte/lackluster.nvim',
   'rose-pine/neovim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
 
   init = function()
-    -- Load the colorscheme here.
-    vim.o.background = 'dark'
-    -- vim.cmd.colorscheme 'nordic'
-    vim.cmd.colorscheme 'rose-pine'
+    require('rose-pine').setup {
+      dark_variant = 'main',
+    }
 
-    -- Enable transparent background
-    vim.cmd [[highlight Normal ctermbg=NONE guibg=NONE]]
-    vim.cmd [[highlight NonText ctermbg=NONE guibg=NONE]]
+    vim.o.background = 'dark'
+
+    vim.cmd.colorscheme 'rose-pine'
 
     -- Enbale true color support
     vim.o.termguicolors = true
