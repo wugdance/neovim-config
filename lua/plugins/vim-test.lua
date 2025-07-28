6379ff976ffa187aa0a05d9879d565136a0749a4
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-07-28 10:14:15 +0300

    add undotree, delete render-markdown

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
 vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { desc = '[T]est [S]uite' })
 vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' })
 vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>', { desc = '[T]est [V]isit' })
+
 vim.cmd "let test#strategy = 'vimux'"
 vim.cmd "let g:VimuxHeight = '50%'"
 vim.cmd "let test#python#pytest#options = '-s -v'"
