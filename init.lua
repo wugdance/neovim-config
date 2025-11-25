3f00b3a17cd7d147e83f908642d39f372fbbccc4
Ryabyshkin Aleksandr Vladimirovich <aryabyshkin@ozon.ru>
2025-11-25 18:42:48 +0300

    del icons part

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ require 'commands'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-bootstrap'
-require('lazy').setup({
+require('lazy').setup {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   require 'plugins.nvim-tmux-navigation',
   require 'plugins.vim-test',
@@ -51,27 +51,7 @@ require('lazy').setup({
   require 'plugins.treesitter',
   require 'plugins.mini',
   require 'plugins.autopairs',
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
